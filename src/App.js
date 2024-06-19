4e142eaf5341694fa21dd4a08e568938e6a7cfaf
ella choy <egal21de@gmail.com>
2024-06-19 12:21:45 +0200

    Erzeugt die Ausgabe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='container'>
+      <h1>Main Content</h1>
+      <form>
+        <ul className='flex-outer'>
+          <li className='such-input'>
+            <label for='first-name'>Such SNR</label>
+            <input type='text' id='first-name' placeholder='Enter SNR Number' />
+          </li>
+          <div className='divider'></div>
+          <li>
+            <label for='last-name'>Aktuelles SNR</label>
+            <input type='text' id='last-name' />
+          </li>
+          <li>
+            <label for='email'>Label 1</label>
+            <input type='text' />
+          </li>
+          <li>
+            <label for='phone'>Label 2</label>
+            <input type='text' />
+          </li>
+          <li>
+            <label for='email'>Label 3</label>
+            <input type='text' />
+          </li>
+          <li>
+            <label for='phone'>Label 4</label>
+            <input type='text' />
+          </li>
+          {/* <li>
+            <label for='message'>Message</label>
+            <textarea
+              rows='6'
+              id='message'
+              placeholder='Enter your message here'
+            ></textarea>
+          </li> */}
+          {/* <li>
+            <p>Age</p>
+            <ul class='flex-inner'>
+              <li>
+                <input type='checkbox' id='twenty-to-twentynine' />
+                <label for='twenty-to-twentynine'>20-29</label>
+              </li>
+              <li>
+                <input type='checkbox' id='thirty-to-thirtynine' />
+                <label for='thirty-to-thirtynine'>30-39</label>
+              </li>
+              <li>
+                <input type='checkbox' id='fourty-to-fourtynine' />
+                <label for='fourty-to-fourtynine'>40-49</label>
+              </li>
+              <li>
+                <input type='checkbox' id='fifty-to-fiftynine' />
+                <label for='fifty-to-fiftynine'>50-59</label>
+              </li>
+              <li>
+                <input type='checkbox' id='sixty-to-sixtynine' />
+                <label for='sixty-to-sixtynine'>60-69</label>
+              </li>
+              <li>
+                <input type='checkbox' id='other' />
+                <label for='other'>Other</label>
+              </li>
+            </ul>
+          </li> */}
+          {/* <li>
+            <button type='submit'>Submit</button>
+          </li> */}
+        </ul>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
